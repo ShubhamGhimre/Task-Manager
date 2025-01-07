584121410e3399fc53ad12a1bbdeae37240fd5ae
Shubham Ghimire <shubham0011ghimire@gmail.com>
2025-01-07 14:52:43 +0545

    Edit Page Popup added

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,61 +1,44 @@
 // app/tasks/page.tsx
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useTaskContext } from "@/context/TaskContext";
-import { Task } from "@/app/types";
-import TaskForm from "@/components/TaskForm";
 import TaskList from "@/components/TaskList";
+import { Search } from "lucide-react";
 
 const TasksPage: React.FC = () => {
   const { tasks, updateTask, deleteTask } = useTaskContext();
-  const [editingTask, setEditingTask] = useState<Task | null>(null);
-
-  const handleEdit = (task: Task) => {
-    setEditingTask(task);
-  };
 
   return (
-    <div className="">
-      <div className="flex justify-center items-center p-10">
-        {editingTask && (
-          <TaskForm
-            onAddTask={() => {}}
-            onUpdateTask={updateTask}
-            editingTask={editingTask}
-            cancelEdit={() => setEditingTask(null)}
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="text-3xl font-semibold mb-4 text-center p-10 ">
+        Task List
+      </h2>
+
+      {/* search and filter */}
+      <div className="flex justify-between w-screen items-center p-4 px-[13%]">
+        <div className="relative flex items-center">
+          <span className="absolute left-3 text-slate-700">
+            <Search size={20} />
+          </span>
+          <input
+            type="text"
+            placeholder="Search Task"
+            className="w-full pl-10 border-2 text-slate-700 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-slate-900"
           />
-        )}
+        </div>
+        <select className="" name="filter" id="filter">
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="pending">Pending</option>
+        </select>
       </div>
 
-      {/* <ul className="list-disc pl-6 mt-4">
-        {tasks.map((task) => (
-          <li key={task.id} className="mb-2">
-            <div className="flex items-center justify-between">
-              <div>
-                <h2 className="font-semibold">{task.title}</h2>
-                <p>{task.description}</p>
-              </div>
-              <div>
-                <button
-                  onClick={() => handleEdit(task)}
-                  className="text-blue-500 hover:underline mr-2"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={() => deleteTask && deleteTask(task.id)}
-                  className="text-red-500 hover:underline"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul> */}
-
       {deleteTask && (
-        <TaskList tasks={tasks} onEdit={handleEdit} onDelete={deleteTask} />
+        <TaskList
+          tasks={tasks}
+          onEdit={updateTask || (() => {})}
+          onDelete={deleteTask}
+        />
       )}
     </div>
   );
